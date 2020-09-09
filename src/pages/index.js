273de2d38a92f89543acb11892babac91819eab9
Austin Cook <74link47@gmail.com>
2020-09-09 18:22:50 -0400

    testing dist

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,29 @@ api.getCardList().then((res) => {
     items: res,
     renderer: (data) => { 
        
+        const card = new Card ({
+            data,
+            handleCardClick:()=>{
+                imagePopup.open({data})
+            }, 
+            handleTrashClick: (id) => {
+                toggleModal(deletePopout) 
+                deleteInput.value = id;
+                deleteForm.setDeleteHandle(()=>{card.removeCard()})
+            },
+            handleLikeClick: (id) => {
+                if (card.likeButton.classList.contains("gallery__like-button_active")){
+                    card.removeLike();
+                    api.changeLikeCardStatus( id, false)
+                    .then(res => card.setLikeCount(res.likes.length));                           
+                } else {
+                    card.addLike();    
+                    api.changeLikeCardStatus(id, true)
+                    .then(res => card.setLikeCount(res.likes.length));
+                }
+            }  
+        }, "#gallery-object");
+
         card.setLikeCount(data.likes.length);
         cardList.addItem(card.generateCard());
         api.getUserInfo()
@@ -33,7 +56,7 @@ api.getCardList().then((res) => {
                 card.hideTrash()
                 
             } 
-            data.likes.some((thing) => {if (thing._id == res._id) {
+            data.likes.some((thing) => {if (thing._id == res._id){
                 card.addLike();
             }});
 
@@ -47,7 +70,7 @@ console.log("testing");
 const galleryForm = new PopupWithForm({popupSelector:galleryPopout, formSubmission: () => {
     setButtonText(galleryPopout, "Saving...")
     api.addCard({name: titleInput.value, link: imageInput.value}).then((res)=>{
-    const newCard = new card ({
+    const newCard = new Card ({
         data: res, 
         handleCardClick:(data) => {
             imagePopup.open({data})}, 
